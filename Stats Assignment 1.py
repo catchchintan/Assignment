bb4b2b01523614ce880b0e087846055ba0c5a714
catchchintan <61616879+catchchintan@users.noreply.github.com>
2020-09-01 17:05:36 -0700

    Add files via upload

---
+++ Stats Assignment 1.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Problem 1
+# Mean = 6.85
+# Median = 7
+# Mode = 7
+# std = 1.59
+
+
+# In[ ]:
+
+
+Problem 2
+# MEan = 107.5
+# MEdian = 100
+# Mode = 75
+# std = 39 
+
+
+# In[ ]:
+
+
+Probel 3
+# mean = 2.15
+# variance = 3.31
+
+
+# In[ ]:
+
+
+# If a part with diameter > 12.6 mm needs to be scrapped,
+# what is the proportion of those parts? - 0.907
+# (need help solving this)
+
+
+# In[ ]:
+
+
+Problem 5
+# Probability of 2 faulty LED = 32%
+# averageg = 1.8
+# std = 1.12
+
+
+# In[ ]:
+
+
+Problem 6
+# Gaurav solves 5 questions correctly - 20.76% probability
+# Barkha solves 5 questions correctly - 22.24% Probability
+# G solves 4 & 6 correctly - 40%
+# B solves 4 & 6 correctly - 38%
+# Infer - Probability of solveing 5 correct questions is almost same for both G & B.
+# Two factors affecting their ability is 1) no. of questions solving per day 2) success rate.
+
+
+# In[ ]:
+
+
+# problem 7
+#Customers arrive at a rate of 72 per hour to my shop. What is the probability of k
+#customers arriving in 4 minutes? a) 5 customers, b) not more than 3 customers, c)
+#more than 3 customers. Give a pictorial representation of the same to validate your
+#answer.
+
+# a) 17%
+# b) 29%
+# c) 71%
+# d) 
+
+
+# In[ ]:
+
+
+# problem 8
+#Let x is the number of errors per page(avg) in an hour , 0.001299
+# for 455 pages, avg erros will be 0.590 (0.001299*455)
+# P(2) = 9.7% @ 455 pages
+# P(2) = 23% @ 1000 pages
+# P(2) = 3.9% @ 255 pages
+# How is the λ affected? - λ increases as number of pages increases. (errors/pages)
+
+
+
+# In[ ]:
+
+
+# problem 10
+# P(Z>1.26) = 1-P(Z<1.26) = 0.10838
+# P(Z<-.86) = 0.19489
+#P(−1.25 < Z < 0.37) = 0.538
+# P(Z ≤ −4.6) = 0
+
+# 10B
+#P(Z > z) = 0.05 => -1.64
+
+#10C
+# P(−z < Z < z)= 0.99 => -3.29 < Z < 3.29
+
+
+# In[ ]:
+
+
+# problem 11
+# P(X>13) = 0.0661
+# P(9 < x < 11) = 0.38292
+# P(0.95) , x = 14.12
+
+
+# In[ ]:
+
+
+# problem 12
+# Proportion of shaft in synch with specification = 92%
+# if mean is 0.250, proportion is 99.8%
+
